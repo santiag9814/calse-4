63ca0d53512b4dd94d126d917836cd4f5a2a6132
santiag9814 <santiago.dunigarro@udea.edu.co>
2026-08-13 17:48:15 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,12 @@
+#a = int(input( "Ingrese un valor: "))
+#b = int(input( "ingrese un valor: "))
+#c = int(input( " ingrese un valor: "))
+#print( " salario actual= {}\n este es salaria actual= {}\n este es el salario actual={}".format(a*0.1 + a , b*0.12+b, c*0.15+c))
+
+1 == int(input("nota "))
+2 == int(input("nota "))
+3 == int(input("nota "))
+4 == int(input("nota "))
+5 == int(input("nota "))
+
+print("promedio= {}." .format((1+2+3+4+5)/5))
